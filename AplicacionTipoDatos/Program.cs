a6e219f139e5ae4b17aaab7405d1d8c7392df3f0
Wilson Hernande Neira Mija <wilsonnm22@gmail.com>
2021-03-20 09:21:52 -0500

    subida de proyectos iniciales C#

---
+++ AplicacionTipoDatos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionTipoDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte num = 255;
+            int numero = 35125;
+            float pi = 3.14156655F;
+            double doble = 3.145566543434;
+            decimal dinero = 434343.51M;
+            char letra = 'D';
+            bool bandero = true;
+            String cadena = "Mi string";
+
+            Console.WriteLine(num);
+            Console.WriteLine(numero);
+            Console.WriteLine(pi);
+            Console.WriteLine(doble);
+            Console.WriteLine(dinero);
+            Console.WriteLine(letra);
+            Console.WriteLine(bandero);
+            Console.WriteLine(cadena);
+
+            double resultado = 27 + 3 * 5 - 16 / 2;
+            Console.WriteLine(resultado);
+            Console.Read();
+        }
+    }
+}
---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo C#"); //imprime una linea en consola
+            Console.WriteLine("5 * 7 = " + (5*7));
+            Console.WriteLine("Fin");
+            Console.Read(); //detiene la ejecucion esperando el ingreso de un valor
+        }
+    }
+}
